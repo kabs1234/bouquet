a6be0a3db7a5bda8845cebde3e298424242ae71f
Aikhan Zhashkeyev <spy.04@mail.ru>
2024-11-23 15:05:49 +0500

    refactor setFilterColor

--- src/models/filters-model.js
+++ src/models/filters-model.js
@@ -22,33 +22,31 @@ export default class FiltersModel extends Observable {
     if (newFilterColor === FilterColor.All) {
       this.#filterColors = [FilterColor.All];
       this._notify(updateType);
-    } else {
-      if (this.filterColors.length === 1 && this.filterColors[0] === FilterColor.All) {
-        this.#filterColors = [newFilterColor];
-        this._notify(updateType);
-        return;
-      }
-
-      if (this.filterColors.includes(newFilterColor)) {
-
-        if (this.filterColors.length === 1 && this.filterColors[0] !== FilterColor.All) {
-          return;
-        }
-
-        const deletingFilterColor = this.#filterColors.indexOf(newFilterColor);
-
-        this.#filterColors = [
-          ...this.#filterColors.slice(0, deletingFilterColor),
-          ...this.#filterColors.slice(deletingFilterColor + 1)
-        ];
-
-        this._notify(updateType);
-        return;
-      }
-
-      this.#filterColors.push(newFilterColor);
-      this._notify(updateType);
+      return;
     }
 
+    const isAllFilterActive = this.filterColors.length === 1 && this.filterColors[0] === FilterColor.All;
+
+    if (isAllFilterActive) {
+      this.#filterColors = [newFilterColor];
+      this._notify(updateType);
+      return;
+    }
+
+    const colorIndex = this.filterColors.indexOf(newFilterColor);
+
+    if (colorIndex !== -1) {
+      if (this.filterColors.length === 1) {
+        return;
+      }
+
+      this.#filterColors.splice(colorIndex, 1);
+      this._notify(updateType);
+      return;
+    }
+
+    this.#filterColors.push(newFilterColor);
+    this._notify(updateType);
   };
+
 }
